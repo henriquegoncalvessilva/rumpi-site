7463d6191d6ce4818ef142d9467bf7de5a004eb0
Biro <luizhenriquegoncalvessilva@hotmail.com>
2026-02-20 15:13:47 -0300

    feat: add animations for mosaic items and dynamic content rendering in script.js

---
+++ js/animations.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+
+  const mosaicItems = document.querySelectorAll('.mosaic-item');
+  
+  mosaicItems.forEach((item, index) => {
+    gsap.fromTo(item,
+      {
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        ease: "elastic.out(1, 1)",
+        scrollTrigger: {
+            markers: true,
+          trigger: item,
+          start: "top 80%",
+          end: "top 50%",
+          toggleActions: "play none none reverse",
+        },
+        delay: index * 0.2
+      }
+    );
+  });
+
+ });
